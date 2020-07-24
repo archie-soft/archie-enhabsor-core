36e015095a44201b10ac04643a1c524a14c00eb9
hilel14 <hilel@elitebook.hilel14.net>
2020-07-24 07:39:58 +0300

    change apache configuration

--- src/setup/docker/ReadMe.md
+++ src/setup/docker/ReadMe.md
@@ -1,12 +1,16 @@
 ## Requirements
-* Download Apache Solr and save in download folder as solr.tgz
-* Download Apache ActiveMQ and save in download folder as activemq.tar. gz
+* Download Apache Solr and save in download folder
+* Download Apache ActiveMQ and save in download folder
 
 ## Build
 sudo docker build --rm --tag local/archie.enhabsor:2 .
 
 ## Run
-sudo docker run  --interactive --detach --tty --name=archie.enhabsor.2 local/archie.enhabsor:2
+sudo docker run -idt --name=archie.enhabsor.2 -p 80:80 -p 8983:8983 -p 8161:8161 local/archie.enhabsor:2
+-i, --interactive 
+-d, --detach 
+-t, --tty 
+-p, --publish
 
 ## Connect
 sudo docker exec -it archie.enhabsor.2 /bin/bash
@@ -16,7 +20,18 @@ sudo docker exec -it archie.enhabsor.2 /bin/bash
 * Edit /opt/apache/activemq/conf/jetty.xml > bean id="jettyPort" >  property name="host" : set value to 0.0.0.0
 
 ## Test
-Replace 172.17.0.2 with real container ip
+
+Docker published ports
+* http://localhost
+* http://localhost:8983/solr
+* http://localhost:8161/admin/ (user: admin, password: admin)
+
+Proxy
+* http://localhost/api
+* http://localhost/docs
+
+Direct access (replace 172.17.0.2 with real container ip)
 * http://172.17.0.2
 * http://172.17.0.2:8983/solr
-* http://172.17.0.2:8161/admin/ (user: admin, password: admin)
+* http://172.17.0.2:8161/admin/
+* mysql -h 172.17.0.2 -u archie -p enhabsor
--- src/setup/docker/setup.sh
+++ src/setup/docker/setup.sh
@@ -3,25 +3,23 @@
 # Exit on first error
 set -e
 
+# Create directories
+mkdir /opt/apache
+for d in assetstore import logs work; do
+    mkdir -p /var/opt/archie/enhabsor/$d
+done
+for d1 in public private secret; do
+    mkdir /var/opt/archie/enhabsor/assetstore/$d1;
+    for d2 in originals thumbnails text; do
+        mkdir /var/opt/archie/enhabsor/assetstore/$d1/$d2;
+    done
+done
+
 # Install core packages
 apt-get update
 apt-get -y install apache2 mariadb-server openjdk-11-jre lsof util-linux imagemagick ghostscript tesseract-ocr tesseract-ocr-heb
 # Install optional packages
 apt-get -y install vim bash-completion iproute2 curl
-
-# Create directory tree
-mkdir /opt/apache
-mkdir /var/opt/archie/
-for d in assetstore import logs work; do
-    mkdir /var/opt/archie/$d
-done
-for d1 in public private secret; do
-    mkdir /var/opt/archie/assetstore/$d1;
-    for d2 in originals thumbnails text; do
-        mkdir /var/opt/archie/assetstore/$d1/$d2;
-    done
-done
-chown -R www-data /var/opt/archie
 
 # Solr configuration
 tar xf ./download/solr-7.7.3.tgz
@@ -43,10 +41,17 @@ chown -R www-data /opt/apache/activemq
 service mysql start
 mysqladmin -u root create enhabsor
 cat ./archie.mariadb.sql | mysql -u root enhabsor
-mysql -u root -e "GRANT ALL ON enhabsor.* TO 'archie'@'localhost' IDENTIFIED BY '1234'"
+mysql -u root -e "GRANT ALL ON enhabsor.* TO 'archie'@'%' IDENTIFIED BY '1234'"
 
 # Apache http server configuration
-mv ./archie.virtual-host.conf /etc/apache2/sites-enabled/
+mkdir -p /var/www/archie/enhabsor
+mv ./index.html /var/www/archie/enhabsor
+mv ./archie.virtual-host.conf /etc/apache2/sites-available/
+chown -R www-data /var/opt/archie/enhabsor
+a2enmod proxy
+a2enmod proxy_http
+a2dissite 000-default
+a2ensite archie.virtual-host
 
 # Move start script to the root of the file system and make sure it is executable
 mv ./start.sh /
